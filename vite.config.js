e1040a9ea1a2153390be51e75448f6d4c96d2cfd
Deep <deepikabv8@gmail.com>
2024-11-12 15:38:16 +0530

    Spotify comment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/https://github.com/deepikaa78/Spotify_clone.git/',
   plugins: [react()],
 })
